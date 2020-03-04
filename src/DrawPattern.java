07b15677906db5b310491749d8680e3c398e467c
madhu-peri <madhup.peri@gmail.com>
2020-03-03 18:57:13 -0600

    Parts (a-d) completed

--- src/DrawPattern.java
+++ src/DrawPattern.java
@@ -1,7 +1,7 @@
 /**
  * Starter code for homework 2 Problem 2 - CIS 2348
- * Name:
- * PS:
+ * Name: Madhumitha Periyasamy
+ * PS: 1595200
  */
 
 import java.util.Scanner;
@@ -11,7 +11,8 @@ public class DrawPattern {
         Scanner scnr = new Scanner(System.in);
         int arrowBaseHeight;
         int arrowBaseWidth;
-        int arrowHeadWidth;
+        int arrowHeadWidth = 0;
+        int i = 0;
 
         System.out.println("Enter arrow base height:");
         arrowBaseHeight = scnr.nextInt();
@@ -19,19 +20,32 @@ public class DrawPattern {
         System.out.println("Enter arrow base width:");
         arrowBaseWidth = scnr.nextInt();
 
+        while (arrowHeadWidth<= arrowBaseWidth) {
         System.out.println("Enter arrow head width:");
-        arrowHeadWidth = scnr.nextInt();
-        System.out.println("");
+            arrowHeadWidth = scnr.nextInt();
+        }
 
-        // Draw arrow base (height = 3, width = 2)
-        System.out.println("**");
-        System.out.println("**");
-        System.out.println("**");
+        String star = "";
+        for (int x = 1; x <= arrowBaseWidth; x++)
+        {
+            star += "*";
+        }
 
-        // Draw arrow head (width = 4)
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        for (i = 1; i <= arrowBaseHeight; ++i)
+        {
+            System.out.println(star);
+        }
+
+        int tempHeadWidth = arrowHeadWidth;
+
+        for (int y = 1; y <= arrowHeadWidth; y++)
+        {
+            for(int z = tempHeadWidth; z > 0; z--)
+            {
+                System.out.print("*");
+            }
+            tempHeadWidth -= 1;
+            System.out.println();
+        }
     }
 }
